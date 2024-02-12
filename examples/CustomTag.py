e91cc98b2ff7811c7c05e4acf1a00b3ad5e9d936
laura-borghesi-lum00n <laura.borghesi@dvisionlab.com>
2024-02-12 15:44:53 +0100

    test custom tag pt.2

--- examples/CustomTag.py
+++ examples/CustomTag.py
@@ -8,18 +8,19 @@ import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Construct the path to the DICOM file relative to the script's directory
-fpath = os.path.join(script_dir, "../testlungo.dcm")
+fpath = os.path.join(script_dir, "../testlungo2.dcm")
 print(fpath)
 ds = dcmread(fpath)
 print(ds)
 # Modify the patient's name (tag: (0010, 0010))
-new_patient_name = "pippoepaperinovannoi"
-print(ds[0x10,0x10].value)
-ds[0x10,0x10].value = new_patient_name
-print(ds[0x10,0x10].value)
+ds[0x10,0x10].value = "knixp"
+ds.StudyDescription = "knixpgblgjbsxglob"
+
+
 
 # Save the modified dataset to a new DICOM file
-new_file_path = "../modified_testlungo.dcm"
+new_file_path = "../modified_testlungo3.dcm"
 ds.save_as(new_file_path)
 
 print("Modified DICOM file saved to:", new_file_path)
+# python3 ./examples/CustomTag.py
